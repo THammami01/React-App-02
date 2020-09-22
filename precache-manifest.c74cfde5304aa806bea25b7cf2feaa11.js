24a6e1aa0f15786c413fa4e14dfb18236bb92e42
Tarek Hammami <thammami.me@gmail.com>
2020-09-22 18:57:34 +0100

    Updates

---
+++ precache-manifest.c74cfde5304aa806bea25b7cf2feaa11.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04c07e23f92ac61c8223edc56b81d28a",
+    "url": "/React-App-02/index.html"
+  },
+  {
+    "revision": "82a6ef9a3822c9d7b3c4",
+    "url": "/React-App-02/static/css/main.e375bcc8.chunk.css"
+  },
+  {
+    "revision": "3788dd95357404788ad5",
+    "url": "/React-App-02/static/js/2.5bf06b29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-App-02/static/js/2.5bf06b29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82a6ef9a3822c9d7b3c4",
+    "url": "/React-App-02/static/js/main.530e2ac5.chunk.js"
+  },
+  {
+    "revision": "d1ffbfeb4e431acd1e73",
+    "url": "/React-App-02/static/js/runtime-main.2e74a980.js"
+  },
+  {
+    "revision": "9b94499ccea3bd82b24cb210733c4b5e",
+    "url": "/React-App-02/static/media/Pacifico-Regular.9b94499c.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/React-App-02/static/media/Roboto-Regular.11eabca2.ttf"
+  }
+]);
